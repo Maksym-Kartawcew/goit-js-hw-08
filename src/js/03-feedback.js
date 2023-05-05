c90a51fae192298469917db5bedcba8db89e2cba
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-05-05 16:14:31 +0200

    homework8_v2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,54 @@
 import throttle from 'lodash.throttle';
+const feedbackForm = document.querySelector('.feedback-form');
 
-const feedbackForm = document.querySelector(".feedback-form");
-const email = feedbackForm.elements.email.value;
-const password = feedbackForm.elements.password.value;
+// list of listeners
 
-feedbackForm.addEventListener("submit", (event) => {
-event.preventDefault();
+feedbackForm.addEventListener('input', throttle(inputFunction, 500));
+feedbackForm.addEventListener('submit', submitFuntion);
 
+//function for saving current values of the form fields
+// (key for the storage "feedback-form-state")
 
-if (!email || !password) {
-alert("Please fill in all fields");
-return;
+function inputFunction(event) {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-const formData = {
-email: email,
-password: password,
-};
+// function for submiting form
+function submitFuntion(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  if (email.value === '' || message.value === '') {
+    window.alert('Fill in all the fields');
+    return;
+  }
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  feedbackForm.reset();
+}
 
-console.log(formData);
-feedbackForm.reset();
-});
+// function for checking the storage and filling fields after page-update
+
+function updatePageFields() {
+  const storageData = localStorage.getItem('feedback-form-state');
+  const parsedStorageData = JSON.parse(storageData);
+  const email = feedbackForm.elements.email;
+  const message = feedbackForm.elements.message;
+  email.value = parsedStorageData.email || '';
+  message.value = parsedStorageData.message || '';
+}
+
+updatePageFields();
