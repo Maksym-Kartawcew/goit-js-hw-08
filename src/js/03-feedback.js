a0ff46fa1f5e0914e211207291648f771830e800
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-05-09 20:11:47 +0200

    03-test version

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,24 @@
 import throttle from 'lodash.throttle';
+
 const feedbackForm = document.querySelector('.feedback-form');
+const formData = {};
 
-// list of listeners
-
-feedbackForm.addEventListener('input', throttle(inputFunction, 100));
-feedbackForm.addEventListener('submit', submitFuntion);
-
+feedbackForm.addEventListener('input', throttle(inputFunction, 500));
+feedbackForm.addEventListener('submit', submitFunction);
 
 //function for saving current values of the form fields
-// (key for the storage "feedback-form-state")
-
-
 function inputFunction(event) {
-  const { currentTarget } = event;
-  if (!currentTarget) return;
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
-  const formData = {
-    email: email.value,
-    message: message.value,
-  };
-
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  formData[event.target.name] = event.target.value;
+  const feedbackData = JSON.stringify(formData);
+  localStorage.setItem('feedback-form-state', feedbackData);
 }
 
-
-// function for submiting form
-function submitFuntion(event) {
+// function for submitting form
+function submitFunction(event) {
   event.preventDefault();
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
+  const { email, message } = event.currentTarget.elements;
   if (email.value === '' || message.value === '') {
-  alert('Fill in all the fields');
+    alert('Fill in all the fields');
     return;
   }
   const formData = {
@@ -46,18 +31,69 @@ function submitFuntion(event) {
 }
 
 // function for checking the storage and filling fields after page-update
-
 function updatePageFields() {
   const storageData = localStorage.getItem('feedback-form-state');
   const parsedStorageData = JSON.parse(storageData);
-  const email = feedbackForm.elements.email;
-  const message = feedbackForm.elements.message;
-  if (email && message) {
-    email.value = parsedStorageData?.email ?? '';
-    message.value = parsedStorageData?.message ?? '';
+  if (parsedStorageData) {
+    const { email, message } = feedbackForm.elements;
+    email.value = parsedStorageData.email || '';
+    message.value = parsedStorageData.message || '';
   }
 }
 
-updatePageFields()
+updatePageFields();
 document.addEventListener('DOMContentLoaded', updatePageFields);
 
+
+// import throttle from 'lodash.throttle';
+
+// const feedbackForm = document.querySelector('.feedback-form');
+// const emailEl = document.querySelector('input');
+// const messageEl = document.querySelector('textarea');
+// const STORAGE_KEY = 'feedback-form-state';
+
+// // List of listeners
+// feedbackForm.addEventListener('input', throttle(inputFunction, 500));
+// feedbackForm.addEventListener('submit', submitFunction);
+
+// // Function for saving current values of the form fields
+// function inputFunction(event) {
+//   let feedbackFormData = localStorage.getItem(STORAGE_KEY);
+//   if (feedbackFormData) {
+//     feedbackFormData = JSON.parse(feedbackFormData);
+//   } else {
+//     feedbackFormData = {};
+//   }
+//   feedbackFormData[event.target.name] = event.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
+// }
+
+// // Function for submitting form
+// function submitFunction(event) {
+//   event.preventDefault();
+//   const { elements: { email, message } } = event.currentTarget;
+//   if (email.value === '' || message.value === '') {
+//     alert('Fill in all the fields');
+//     return;
+//   }
+//   const formData = {
+//     email: email.value,
+//     message: message.value,
+//   };
+//   console.log(formData);
+//   localStorage.removeItem(STORAGE_KEY);
+//   feedbackForm.reset();
+// }
+
+// // Function for checking the storage and filling fields after page-update
+// function fillFormFields() {
+//   const savedFormData = localStorage.getItem(STORAGE_KEY);
+//   if (savedFormData) {
+//     const parsedFormData = JSON.parse(savedFormData);
+//     emailEl.value = parsedFormData.email ? parsedFormData.email : '';
+//     messageEl.value = parsedFormData.message ? parsedFormData.message : '';
+//   }
+// }
+
+// fillFormFields();
+// document.addEventListener('DOMContentLoaded', fillFormFields);
